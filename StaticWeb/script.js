986dbea8c860678db71a97b0d2583acdf0a0fbfe
CH_Vithanage <kaplakmali76@gmail.com>
2024-11-16 00:26:31 +0530

    Upgraed navbar

--- StaticWeb/script.js
+++ StaticWeb/script.js
@@ -1,12 +1,1 @@
-// function toggleNavMenu() {
-//   const hamburger = document.getElementById('hamburger');
-//   hamburger.classList.add('hamburger-fade');
-//   setTimeout(() => {
-//     if (hamburger.textContent === 'menu') {
-//       hamburger.textContent = 'close';
-//     } else {
-//       hamburger.textContent = 'menu';
-//     }
-//     hamburger.classList.remove('hamburger-fade');
-//   }, 300);
-// }
+
